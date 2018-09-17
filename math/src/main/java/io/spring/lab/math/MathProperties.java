d1f7ea05430266d0c4f8f83876294fb567790c16
Darek Kaczyński <kaczynskid@gmail.com>
2018-09-17 14:35:16 +0200

    Extracted math common module from marketing

--- math/src/main/java/io/spring/lab/math/MathProperties.java
+++ math/src/main/java/io/spring/lab/math/MathProperties.java
@@ -2,17 +2,20 @@ package io.spring.lab.math;
 
 import java.math.MathContext;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import lombok.Data;
 
 import static java.math.RoundingMode.HALF_EVEN;
 
 @Data
+@ConfigurationProperties(prefix = "math")
 public class MathProperties {
 
-	/** Precision for Math operations */
+	/** Precision for math operations */
 	private int precision = 18;
 
-	/** Scale for Math operations */
+	/** Scale for math operations */
 	private int scale = 4;
 
 	public MathContext getContext() {
