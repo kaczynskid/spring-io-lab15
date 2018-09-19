ea201db2a00fc3fbffad8c6669ba98198b35ad55
Darek Kaczyński <kaczynskid@gmail.com>
2018-09-19 09:35:19 +0200

    Added getItems contract for warehouse

---
+++ warehouse/src/test/resources/contracts/shouldGetItem.groovy
@@ -0,0 +1,25 @@
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        name('get item')
+        method('GET')
+        url('/items/1')
+        headers {
+            header('Accept', 'application/json;charset=UTF-8')
+        }
+    }
+    response {
+        status 200
+        headers {
+            header('Content-Type', 'application/json;charset=UTF-8')
+        }
+        body([
+                name: 'A',
+                count: 100,
+                price: 40.0
+        ])
+    }
+}
