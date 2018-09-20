f099b77cefb2d446800268298451f9148937833b
Darek Kaczyński <kaczynskid@gmail.com>
2018-09-20 11:17:27 +0200

    Config server from registry

--- marketing/src/test/java/io/spring/lab/marketing/SpringTestBase.java
+++ marketing/src/test/java/io/spring/lab/marketing/SpringTestBase.java
@@ -1,10 +1,13 @@
 package io.spring.lab.marketing;
 
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+        "spring.cloud.bootstrap.enabled=false",
+        "spring.profiles.active=test"
+})
 public abstract class SpringTestBase {
 }
