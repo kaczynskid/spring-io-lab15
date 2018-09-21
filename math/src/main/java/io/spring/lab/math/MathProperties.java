461d07cf6b2b8874d6ae19285f547e704eed251e
Darek Kaczyński <kaczynskid@gmail.com>
2018-09-21 09:39:36 +0200

    Added fallback for special calculation

--- math/src/main/java/io/spring/lab/math/MathProperties.java
+++ math/src/main/java/io/spring/lab/math/MathProperties.java
@@ -2,6 +2,7 @@ package io.spring.lab.math;
 
 import static java.math.RoundingMode.HALF_EVEN;
 
+import java.math.BigDecimal;
 import java.math.MathContext;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -20,5 +21,9 @@ public class MathProperties {
 
 	public MathContext getContext() {
 		return new MathContext(precision, HALF_EVEN);
+	}
+
+	public BigDecimal bigDecimal(long unscaledVal) {
+		return BigDecimal.valueOf(unscaledVal * (long) Math.pow(10, scale), scale);
 	}
 }
--- store/src/main/java/io/spring/lab/store/special/DeclarativeSpecialClient.java
+++ store/src/main/java/io/spring/lab/store/special/DeclarativeSpecialClient.java
@@ -1,5 +1,7 @@
 package io.spring.lab.store.special;
 
+import java.math.BigDecimal;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
@@ -7,20 +9,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import io.spring.lab.math.MathProperties;
 import lombok.AllArgsConstructor;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.TEN;
 
 @Component
 @AllArgsConstructor
 @ConditionalOnProperty(value = "feign.client.enabled", havingValue = "true", matchIfMissing = true)
 public class DeclarativeSpecialClient implements SpecialClient {
 
-    @FeignClient(name = "marketing", path = "/specials")
+    @FeignClient(name = "marketing", path = "/specials", fallback = FallbackSpecialClient.class)
     interface FeignSpecialClient {
 
         @PostMapping("/{itemId}/calculate")
         SpecialCalculation calculateFor(
                 @PathVariable("itemId") long itemId,
                 @RequestBody SpecialCalculationRequest request);
+    }
+
+    @Component
+    @AllArgsConstructor
+    static class FallbackSpecialClient implements FeignSpecialClient {
+
+        private MathProperties math;
+
+        @Override
+        public SpecialCalculation calculateFor(long itemId, SpecialCalculationRequest request) {
+            BigDecimal unitCount = math.bigDecimal(request.getUnitCount());
+            BigDecimal regularPrice = request.getUnitPrice().multiply(unitCount, math.getContext());
+            BigDecimal discountRate = ONE.divide(TEN, math.getContext());
+            BigDecimal discount = regularPrice.multiply(discountRate, math.getContext());
+            BigDecimal fallbackPrice = regularPrice.subtract(discount, math.getContext());
+            return new SpecialCalculation("fallback", fallbackPrice);
+        }
     }
 
     private FeignSpecialClient specials;
